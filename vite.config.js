6e1059b3f37ca6873cbe9df3cf3ed03d1288b44e
Juan A. García <juan.a.garcia.gomez@gmail.com>
2025-09-28 18:21:14 +0200

    fix: url_base & package_name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'src',
-  base: '/api-examples/', // Cambia esto por el nombre de tu repositorio
+  base: '/dragon-ball-universe-explorer/', // Cambia esto por el nombre de tu repositorio
   build: {
     outDir: '../dist',
     emptyOutDir: true
